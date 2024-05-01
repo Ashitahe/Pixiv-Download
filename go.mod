0f62b8c35a142eb125d4e9f3688072177f558a57
Asuhe <kemira543@gmail.com>
2024-05-01 10:54:25 +0800

    feat: optimze download api

--- go.mod
+++ go.mod
@@ -3,8 +3,11 @@ module spider
 go 1.19
 
 require (
+	github.com/go-resty/resty/v2 v2.7.0
 	github.com/gocolly/colly v1.2.0
 	github.com/tidwall/gjson v1.14.4
+	gorm.io/driver/mysql v1.5.1
+	gorm.io/gorm v1.25.3
 )
 
 require (
@@ -14,16 +17,19 @@ require (
 	github.com/antchfx/xmlquery v1.2.4 // indirect
 	github.com/antchfx/xpath v1.1.8 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/go-sql-driver/mysql v1.7.0 // indirect
 	github.com/gobwas/glob v0.2.3 // indirect
 	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
 	github.com/golang/protobuf v1.4.2 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/kennygrant/sanitize v1.2.4 // indirect
 	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
 	github.com/temoto/robotstxt v1.1.1 // indirect
 	github.com/tidwall/match v1.1.1 // indirect
 	github.com/tidwall/pretty v1.2.1 // indirect
-	golang.org/x/net v0.0.0-20200602114024-627f9648deb9 // indirect
-	golang.org/x/text v0.3.2 // indirect
+	golang.org/x/net v0.0.0-20211029224645-99673261e6eb // indirect
+	golang.org/x/text v0.3.6 // indirect
 	google.golang.org/appengine v1.6.6 // indirect
 	google.golang.org/protobuf v1.24.0 // indirect
 )
--- main.go
+++ main.go
@@ -3,47 +3,91 @@ package main
 import (
 	"bufio"
 	"bytes"
+	"crypto/tls"
+	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
-	"net"
+	"math/rand"
 	"net/http"
 	"net/url"
 	"os"
+	"path"
+	"path/filepath"
 	"runtime"
-	"strings"
 	"time"
 
 	"github.com/gocolly/colly"
 	"github.com/tidwall/gjson"
 )
 
-const reverseProxyUrl = "https://pimg.rem.asia"
-
-func saveFile(content []byte, savePath string, fileNmae string) error {
-
-	output, err := os.Create(savePath + fileNmae) // create a file to save file. say..pic.jpg. 				// save. 				// name
-	if err != nil {
-		fmt.Println("create file failed!", err) // name of the file. 				// save. 				// name.)
-		return err
-	}
-	defer output.Close()
-	_, err = io.Copy(output, bytes.NewReader(content)) // save the file. say. 			// name of the file.
-
-	if err != nil {
-		fmt.Println("copy failed!", err) // name of the file. 			// save. 				// name of the file.
-		return err
-	}
-
-	fmt.Println(fileNmae + " downloaded")
-
-	return nil
+type DnsResponse struct {
+	Answer []struct {
+		Data string `json:"data"`
+	} `json:"Answer"`
 }
 
-// 根据插画ID搜索并下载
-func searchByIllustId(id string) error {
-	baseURL := "https://www.pixiv.net/touch/ajax/illust/details?illust_id=" + id
-	savePath := "./illusts_" + id + "/" // path where the image will be saved. say./pixiv_images/
+func getDnsResponse(domain string) string {
 
+	if domain == "" {
+		domain = "www.pixiv.net"
+	}
+
+	urls := []string{
+		"https://1.0.0.1/dns-query",
+		"https://1.1.1.1/dns-query",
+		"https://doh.dns.sb/dns-query",
+		"https://cloudflare-dns.com/dns-query",
+	}
+
+	client := &http.Client{Timeout: 3 * time.Second}
+
+	var dnsResp DnsResponse
+
+	for _, url := range urls {
+		req, _ := http.NewRequest("GET", url, nil)
+		q := req.URL.Query()
+		q.Add("name", domain)
+		q.Add("type", "A")
+		q.Add("do", "false")
+		q.Add("cd", "false")
+		req.URL.RawQuery = q.Encode()
+		req.Header.Add("Accept", "application/dns-json")
+
+		resp, err := client.Do(req)
+		if err != nil {
+			fmt.Println("Unable to establish connection to", url)
+			continue
+		}
+		defer resp.Body.Close()
+
+		err = json.NewDecoder(resp.Body).Decode(&dnsResp)
+		if err != nil {
+			fmt.Println("Unable to get according hostname info from", url)
+			continue
+		}
+
+		if len(dnsResp.Answer) > 0 {
+			fmt.Println("IP address:", dnsResp.Answer[0].Data, "from", dnsResp.Answer)
+		} else {
+			fmt.Println("No IP address found from", url, dnsResp)
+		}
+	}
+
+	return dnsResp.Answer[0].Data
+}
+
+func getRandomElement(list []string) string {
+	rand.Seed(time.Now().UnixNano())
+	return list[rand.Intn(len(list))]
+}
+
+func getProxyHostDomain() string {
+	proxyList := []string{"pimg.rem.asia"}
+	return getRandomElement(proxyList)
+}
+
+func createCollector() *colly.Collector {
 	c := colly.NewCollector(colly.Async(true))
 
 	c.Limit(&colly.LimitRule{
@@ -51,97 +95,199 @@ func searchByIllustId(id string) error {
 		RandomDelay: 5 * time.Second,
 	})
 
-	c.WithTransport(&http.Transport{
-		Proxy: http.ProxyFromEnvironment,
-		DialContext: (&net.Dialer{
-			Timeout:   120 * time.Second,
-			KeepAlive: 30 * time.Second,
-		}).DialContext,
-		MaxIdleConns:          100,
-		IdleConnTimeout:       90 * time.Second,
-		TLSHandshakeTimeout:   10 * time.Second,
-		ExpectContinueTimeout: 1 * time.Second,
-	})
+	// Create a Transport that skips SNI verification
+	tr := &http.Transport{
+		TLSHandshakeTimeout: 1 * time.Minute,
+		TLSClientConfig: &tls.Config{
+			InsecureSkipVerify: true,
+			ServerName:         "",
+		},
+	}
 
-	imgC := c.Clone()
+	// Set the Collector's Transport
+	c.WithTransport(tr)
 
-	imgC.Limit(&colly.LimitRule{
-		Parallelism: runtime.NumCPU(),
-		RandomDelay: 5 * time.Second,
-	})
+	return c
+}
 
-	imgC.OnError(func(r *colly.Response, err error) {
-		fmt.Println("Download illust error", err)
-	})
+func saveFile(saveContent []byte, savePath string, fileName string) error {
+	// create directory if not exists.
+	if err := os.MkdirAll(savePath, os.ModePerm); err != nil {
+		fmt.Println("Error: can't create directory, using current directory instead:", err)
+		savePath = "./"
+	}
 
-	imgC.OnResponse(func(resp *colly.Response) {
-		path := strings.Split(resp.Request.URL.String(), "/")
-		imgName := path[len(path)-1] // name of the image. say. "pixiv_id-date.jpg"
-		saveFile(resp.Body, savePath, imgName)
-	})
+	fullPath := filepath.Join(savePath, fileName)
+	output, err := os.Create(fullPath) // create a file to save file. say..pic.jpg.
+	if err != nil {
+		fmt.Printf("Failed to create file %s: %v\n", fullPath, err)
+		return err
+	}
+	defer output.Close()
 
-	c.OnError(func(r *colly.Response, err error) {
-		fmt.Println("Get illust's detail error", err)
-	})
+	if _, err = io.Copy(output, bytes.NewReader(saveContent)); err != nil {
+		fmt.Printf("Failed to write to file %s: %v\n", fullPath, err)
+		return err
+	}
 
-	c.OnRequest(func(r *colly.Request) {
-		r.Headers.Add("referer", "https://www.pixiv.net/")
-	})
-
-	c.OnResponse(func(r *colly.Response) {
-		res := gjson.Get(string(r.Body), "body.illust_details.manga_a")
-
-		// create directory if not exists.
-		if os.Mkdir(savePath, os.ModePerm) != nil {
-			fmt.Println("Error: can't create directory") // debug only.
-			savePath = "./"
-		}
-
-		if res.Raw == "" {
-			link := gjson.Get(string(r.Body), "body.illust_details.url")
-			parsed, _ := url.Parse(link.Str)
-			imgC.Visit(reverseProxyUrl + parsed.Path)
-		} else {
-			res.ForEach(func(key, value gjson.Result) bool {
-				link := gjson.Get(value.Raw, "url")
-				parsed, _ := url.Parse(link.Str)
-				imgC.Visit(reverseProxyUrl + parsed.Path)
-				return true
-			})
-		}
-	})
-
-	c.Visit(baseURL) // Visit the URL for the desired page. 			// returns immediately. 			// Visit does not
-	c.Wait()
-	imgC.Wait()
+	fmt.Printf("%s downloaded\n", fileName)
 
 	return nil
 }
 
-// 根据画师ID搜索并下载该画师的所有画品
-func searchByUid(uid string) error {
-	baseURL := "https://www.pixiv.net/ajax/user/" + uid + "/profile/all"
+func downloadImage(url string, savePath string) error {
 
-	c := colly.NewCollector()
-	c.OnError(func(r *colly.Response, err error) {
-		fmt.Println("Get user's detail error", err)
-	})
-	c.OnResponse(func(r *colly.Response) {
-		imgs := gjson.Get(string(r.Body), "body.illusts")
-		imgs.ForEach(func(key, value gjson.Result) bool {
-			searchByIllustId(key.Str)
+	imgVisitor := createCollector()
+
+	isError := false
+
+	handleDownloadImgError := func(r *colly.Response, err error) {
+		fmt.Println("Download illust error", err)
+		isError = true
+	}
+
+	handleDownloadImgSuccess := func(resp *colly.Response) {
+		imgName := path.Base(resp.Request.URL.Path)
+		saveFile(resp.Body, savePath, imgName)
+	}
+
+	imgVisitor.OnError(handleDownloadImgError)
+
+	imgVisitor.OnResponse(handleDownloadImgSuccess)
+
+	imgVisitor.Visit(url)
+
+	imgVisitor.Wait()
+
+	if isError {
+		return errors.New("download illust error")
+	} else {
+		return nil
+	}
+}
+
+func processLink(linkStr string) string {
+	parsed, _ := url.Parse(linkStr)
+	originImgUrl := &url.URL{
+		Scheme: "https",
+		Host:   getProxyHostDomain(),
+		Path:   parsed.Path,
+	}
+	return originImgUrl.String()
+}
+
+// 根据插画ID搜索并下载
+func searchByIllustId(id string) error {
+
+	c := createCollector()
+
+	reqUrl := &url.URL{
+		Scheme: "https",
+		Host:   "www.pixiv.net",
+		Path:     "/ajax/illust/" + id + "/pages",
+	}
+
+	saveImgPath := "./illusts_" + id + "/"
+
+	isDownloaded := true
+
+	handleGetIllustInfoSuccess := func(r *colly.Response) {
+		body := string(r.Body)
+		imgObjList := gjson.Get(body, "body.#.urls.original")
+
+		downloadAndCheck := func(link string) {
+			downlandRes := downloadImage(processLink(link), saveImgPath)
+			if downlandRes != nil {
+				isDownloaded = false
+			}
+		}
+
+		imgObjList.ForEach(func(key, value gjson.Result) bool {
+			downloadAndCheck(value.Str)
 			return true
 		})
-	})
+	}
 
-	c.Visit(baseURL)
+	handleGetIllustInfoError := func(r *colly.Response, err error) {
+		fmt.Println("Get illust's detail error", err)
+		isDownloaded = false
+	}
+
+	c.OnResponse(handleGetIllustInfoSuccess)
+
+	c.OnError(handleGetIllustInfoError)
+
+	c.Visit(reqUrl.String())
+
+	c.Wait()
+
+	if isDownloaded {
+		return nil
+	} else {
+		return errors.New("download illust error")
+	}
+}
+
+func getIllusts(jsonData string, jsonPathStr string) []string {
+	imgList := make([]string, 0)
+	illusts := gjson.Get(jsonData, jsonPathStr)
+	illusts.ForEach(func(key, value gjson.Result) bool {
+		imgList = append(imgList, key.Str)
+		return true
+	})
+	return imgList
+}
+
+// 根据画师ID搜索并下载该画师的所有画品
+func searchByUid(uid string) error {
+
+	uidInfoUrl := &url.URL{
+		Scheme: "https",
+		Host:   "www.pixiv.net",
+		Path: "/ajax/user/" + uid + "/profile/all",
+	}
+
+	c := createCollector()
+
+	handleGetUserInfoError := func(r *colly.Response, err error) {
+		fmt.Println("Get user's detail error", err)
+	}
+
+	handleGetUserInfoSuccess := func(r *colly.Response) {
+		imgsRes := getIllusts(string(r.Body), "body.illusts")
+
+		totalImgs := len(imgsRes)
+
+		downloadedImgs := 0
+
+		errImg := make([]string, 0)
+
+		for _, imgId := range imgsRes {
+			if searchByIllustId(imgId) != nil {
+				errImg = append(errImg, imgId)
+			} else {
+				downloadedImgs++
+			}
+		}
+
+		fmt.Printf("Total images: %d, downloaded: %d, failed: %d\n", totalImgs, downloadedImgs, len(errImg))
+		fmt.Println("Failed images: ", errImg)
+	}
+
+	c.OnError(handleGetUserInfoError)
+
+	c.OnResponse(handleGetUserInfoSuccess)
+
+	c.Visit(uidInfoUrl.String())
+
+	c.Wait()
 	return nil
 }
 
 func menu() {
 	fmt.Println("1.Dowanload image by illust's id")
 	fmt.Println("2.Dowanload image by uid")
-	fmt.Println("3.exit")
+	fmt.Println("3.Doh test")
 	fmt.Println("input your option")
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Scan()
@@ -154,6 +300,10 @@ func menu() {
 		fmt.Println("Please enter the uid of the image you want to download: ")
 		scanner.Scan()
 		searchByUid(scanner.Text())
+	case "3":
+		fmt.Println("Doh test domain: ")
+		scanner.Scan()
+		getDnsResponse(scanner.Text())
 	default:
 		fmt.Println("Goodbye!")
 	}
